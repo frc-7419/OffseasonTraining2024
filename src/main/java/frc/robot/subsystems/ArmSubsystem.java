f6f3b949cd60504b496d69d2024a63f5e8a232b4
mqxs21 <maxshi239@gmail.com>
2024-09-12 16:28:25 -0700

    fixeed syntax

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,28 +1,29 @@
 package frc.robot.subsystems;
-
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
 
-  private final TalonFX armMotor;
-  public ArmSubsystem() {
-    this.armMotor = new TalonFX(Constants.ShooterConstants.topShooterID, Constants.RobotConstants.kCanbus);
-    armMotor.setInverted(true);
+private final TalonFX armMotor;
+    public ArmSubsystem() {
+      this.armMotor = new TalonFX(0);
     }
+
     public void coast(){
         armMotor.setNeutralMode(NeutralModeValue.Coast);
     }
     public void brake(){
-      armMotor.setNeutralMode(NeutralModeValue.Brake);
-    }
+        armMotor.setNeutralMode(NeutralModeValue.Brake);
+      }
     public void setPower(double power){
-      armMotor.set(power);
+        armMotor.set(power);
     }
 
-    @Override
-    public void periodic() {
-    }
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("arm motor voltage", armMotor.getMotorVoltage().getValue());
+  }
 }
