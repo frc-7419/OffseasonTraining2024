3106180ed6a13994a3dc7daf9d350b335cb9510a
VivianS27 <143737473+VivianS27@users.noreply.github.com>
2024-09-12 23:07:14 -0700

    Created Subsystem

---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSubsystem extends SubsystemBase {
+
+private final TalonFX armMotor;
+    public ArmSubsystem() {
+      this.armMotor = new TalonFX(0);
+    }
+
+    public void coast(){
+        armMotor.setNeutralMode(NeutralModeValue.Coast);
+    }
+    public void brake(){
+        armMotor.setNeutralMode(NeutralModeValue.Brake);
+      }
+    public void setPower(double power){
+        armMotor.set(power);
+    }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("ArmMotorVoltage",armMotor.getMotorVoltage().getValue());
+  }
+}
+
