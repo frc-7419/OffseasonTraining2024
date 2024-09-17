12f3bbf162ef167ed293d6ca6d0696a512e5150a
Amrit-s-RIS <therealamritsingh@gmail.com>
2024-09-17 12:29:20 -0700

    Revert "made command"
    This reverts commit bb51ecd547c693afbeecb3e138a0564dd6cb261f, reversing
    changes made to 6fd5d95493e0f23566981a6bbe8a53a69efbe507.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,33 +4,21 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.commands.RunArmWithJoystick;
-import frc.robot.subsystems.ArmSubsystem;
 
 public class RobotContainer {
   
   private final CommandXboxController driveJoystick = new CommandXboxController(Constants.Operator.kdriveControllerPort);
-  private final ArmSubsystem arm = new ArmSubsystem();
-  private final RunArmWithJoystick runArmWithJoystick = new RunArmWithJoystick(arm, driveJoystick);
-  private final Command runArmJoystick = new RunCommand(()->arm.runMotor(driveJoystick.getLeftY()), arm);
 
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {
-    driveJoystick.rightBumper().getAsBoolean();
-  }
+  private void configureBindings() {}
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
-  }
-  public void setDefaultCommands() {
-    arm.setDefaultCommand(runArmWithJoystick);
   }
 }
