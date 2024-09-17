248344241bd55c8b062842d3e512452d84e78fdd
RuberrBandsss <167698183+RuberrBandsss@users.noreply.github.com>
2024-09-17 12:58:01 -0700

    Finished ArmSubsystm along with Run Arm Command

---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardwarare.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSubsystem extends SubsystemBase {
+  private final TalonFX armMotor;
+  /** Creates a new ArmSubsystem. */
+  public ArmSubsystem() {
+    this.armMotor = new TalonFX(deviceId:0);
+  }
+
+  public void runMotor(double power){
+    armMotor.set(power);
+  }
+  public void coast(){
+    armMotor.setNeutralMode(NeutralModeValue.Coast);
+  }
+  public void brake(){
+    armMotor.setNeutralMode(NeutralModeValue.Brake);
+  }
+
+
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Arm motor voltage", armMotor.getMotorVoltage().getValue());
+    // This method will be called once per scheduler run
+  }
+}
