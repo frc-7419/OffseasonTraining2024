19e3538b987185e2600ca295b69ade2fe39e0303
shlokacharya <64059790+shlokacharya@users.noreply.github.com>
2024-09-19 12:22:37 -0700

    Subsystem
    (Copied from Original Branch)

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,16 +1,16 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
 public class ArmSubsystem extends SubsystemBase {
+
   private final TalonFX armMotor;
 
   public ArmSubsystem() {
@@ -28,7 +28,7 @@ public class ArmSubsystem extends SubsystemBase {
   public void brake() {
     armMotor.setNeutralMode(NeutralModeValue.Brake);
   }
-
+  
   @Override
   public void periodic() {
     SmartDashboard.putNumber("Arm motor voltage", armMotor.getMotorVoltage().getValue());
