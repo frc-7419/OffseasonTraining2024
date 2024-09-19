10a809e932f067bd41e385166bd196255a6d128e
shlokacharya <64059790+shlokacharya@users.noreply.github.com>
2024-09-19 12:08:17 -0700

    Command - New Branch
    I deleted the code that was already there and coded the Command again, understanding each line of code.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,8 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-
+/* 
+Imports necessary packages to run FRC commands
+*/
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -11,21 +10,22 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.RunArmWithJoystick;
 import frc.robot.subsystems.ArmSubsystem;
 
+// Creating an instance of the different members. 
 public class RobotContainer {
-  
   private final CommandXboxController driveJoystick = new CommandXboxController(Constants.Operator.kdriveControllerPort);
   private final ArmSubsystem arm = new ArmSubsystem();
   private final RunArmWithJoystick runArmWithJoystick = new RunArmWithJoystick(arm, driveJoystick);
   private final Command runArmJoystick = new RunCommand(()->arm.runMotor(driveJoystick.getLeftY()), arm);
 
+
   public RobotContainer() {
     configureBindings();
   }
 
+  private void configureBindings() {}
   private void configureBindings() {
     driveJoystick.rightBumper().getAsBoolean();
   }
-
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
--- src/main/java/frc/robot/commands/RunArmWithJoystick.java
+++ src/main/java/frc/robot/commands/RunArmWithJoystick.java
@@ -1,42 +1,34 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.ArmSubsystem;
 
 public class RunArmWithJoystick extends Command {
-  private final ArmSubsystem arm;
+  private final ArmSubsystem armsubsystem;
   private final CommandXboxController joystick;
 
-  public RunArmWithJoystick(ArmSubsystem arm, CommandXboxController joystick) {
-    this.arm = arm;
+//i
+  public RunArmWithJoystick(ArmSubsystem armsubsystem, CommandXboxController joystick) {
+    this.armsubsystem = armsubsystem;
     this.joystick = joystick;
-    addRequirements(arm);
+    addRequirements(armsubsystem);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    arm.coast();
+    armsubsystem.coast();
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    arm.runMotor(joystick.getLeftY());
+    armsubsystem.runMotor(joystick.getLeftY());
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    arm.brake();
+    armsubsystem.brake();
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
