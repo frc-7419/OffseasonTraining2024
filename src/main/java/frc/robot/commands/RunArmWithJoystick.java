2a62e51e3ae4a1908face44227e47d92a2015307
Bachthoven <bachtoven.rules@gmail.com>
2024-09-19 10:03:16 -0700

    Subsystem, Command, and Robot Container

---
+++ src/main/java/frc/robot/commands/RunArmWithJoystick.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.ArmSubsystem;
+
+public class RunArmWithJoystick extends Command {
+  private final ArmSubsystem arm;
+  private final CommandXboxController joystick;
+
+  public RunArmWithJoystick(ArmSubsystem arm, CommandXboxController joystick) {
+    this.arm = arm;
+    this.joystick = joystick;
+    addRequirements(arm);
+  }
+
+  @Override
+  public void initialize() {
+    arm.coast();
+  }
+
+  @Override
+  public void execute() {
+    arm.runMotor(joystick.getLeftY());
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    arm.brake();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false; 
+  }
+}
