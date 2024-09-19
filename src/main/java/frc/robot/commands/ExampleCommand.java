b7e20abcd8b9efe796484e2590194537dc2e936d
CryptoSOL123 <163804105+CryptoSOL123@users.noreply.github.com>
2024-09-19 16:04:52 -0700

    New Work
    More Work

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -1,15 +1,18 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.ArmSubsystem;
 
 public class ExampleCommand extends Command {
   /** Creates a new ExampleCommand. */
+  private final armSubsystem;
   public ExampleCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirments()
   }
 
   // Called when the command is initially scheduled.
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj2.smartdashboard.SmartDashboard;
 
 //
@@ -12,15 +14,15 @@ public class ArmSubsystem extends SubsystemBase {
     this.armMotor = new TalonFX(Constants.ShooterConstants.topShooterID, Constants.RobotConstants.kCanbus);
       }
 
-      public void coast(){
-        armMotor.setNeutralMode(NeutralModeValue.coast);
-      }
-      public void brake(){
-        armMotor.setNeutralMode(NeutralModeValue.brake);
-      }
-      public setPower(double power){
-        armMotor.set(power);
-      }
+  public void coast(){
+    armMotor.setNeutralMode(NeutralModeValue.Coast);
+  }
+  public void brake(){
+    armMotor.setNeutralMode(NeutralModeValue.Brake);
+  }
+  public setPower(double power){
+    armMotor.set(power);
+  }
 
   @Override
   public void periodic() {
