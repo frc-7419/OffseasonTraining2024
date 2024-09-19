71238349360210947b89df459e51be320acb98d4
humanifying9 <tiaparekh9@gmail.com>
2024-09-18 18:50:02 -0700

    Command task

--- src/main/java/frc/robot/commands/RunArm.java
+++ src/main/java/frc/robot/commands/RunArm.java
@@ -9,20 +9,37 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RunArm extends Command {
   /** Creates a new RunArm. */
   public RunArm() {
+    private final ArmSubsystem arm;
+    public RunArm(ArmSubsystem arm) {
+      this.arm=arm;
+      addRequirements(armSybsystem);
+
+    }
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    arm.coast();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    arm.runMotor
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) { 
+
+
+
+
+
+    
+  }
 
   // Returns true when the command should end.
   @Override
