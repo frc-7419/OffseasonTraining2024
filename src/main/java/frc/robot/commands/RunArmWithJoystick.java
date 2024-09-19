bcc58eb86f9ef12d634e80e1e4938ed4ef5bfa51
shlokacharya <64059790+shlokacharya@users.noreply.github.com>
2024-09-19 14:11:38 -0700

    Naming Convention Fix

--- src/main/java/frc/robot/commands/RunArmWithJoystick.java
+++ src/main/java/frc/robot/commands/RunArmWithJoystick.java
@@ -4,29 +4,29 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.ArmSubsystem;
 
 public class RunArmWithJoystick extends Command {
-  private final ArmSubsystem armsubsystem;
+  private final ArmSubsystem armSubsystem;
   private final CommandXboxController joystick;
 
 //i
-  public RunArmWithJoystick(ArmSubsystem armsubsystem, CommandXboxController joystick) {
-    this.armsubsystem = armsubsystem;
+  public RunArmWithJoystick(ArmSubsystem armSubsystem, CommandXboxController joystick) {
+    this.armSubsystem = armSubsystem;
     this.joystick = joystick;
-    addRequirements(armsubsystem);
+    addRequirements(armSubsystem);
   }
 
   @Override
   public void initialize() {
-    armsubsystem.coast();
+    armSubsystem.coast();
   }
 
   @Override
   public void execute() {
-    armsubsystem.runMotor(joystick.getLeftY());
+    armSubsystem.runMotor(joystick.getLeftY());
   }
 
   @Override
   public void end(boolean interrupted) {
-    armsubsystem.brake();
+    armSubsystem.brake();
   }
 
   @Override
