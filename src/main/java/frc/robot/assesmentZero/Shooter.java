f7bb0b9341a3d2d221edb0bf658ebebb13f22bd7
RoyalStrawberries <adityagandhi2022@gmail.com>
2024-09-20 16:36:11 -0700

    Update Shooter.java

--- src/main/java/frc/robot/assesmentZero/Shooter.java
+++ src/main/java/frc/robot/assesmentZero/Shooter.java
@@ -5,24 +5,51 @@
 package frc.robot.assesmentZero;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class Shooter extends Command {
   /** Creates a new Shooter. */
-  public Shooter() {
+  private final CommandXboxController joystickCommandXboxController;
+  private final ShooterSubsystem shooterMotorShooterSubsystem;
+  private final ShooterSubsystem topMotorShooterSubsystem;
+  private final ShooterSubsystem bottomMotorShooterSubsystem;
+  private final JoystickButton buttonJoystickButton;
+  //the up does the button idk how to import it or do it cuz we never went over it
+
+  public Shooter(ShooterSubsystem shooterMotor, CommandXboxController joystickCommandXboxController, ShooterSubsystem topMotor, ShooterSubsystem bottomMotor, JoystickButton buttonJoystickButton) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.shooterMotorShooterSubsystem = shooterMotor;
+    this.joystickCommandXboxController = joystickCommandXboxController;
+    this.bottomMotorShooterSubsystem = bottomMotor;
+    this.topMotorShooterSubsystem = topMotor;
+    this.buttonJoystickButton = buttonJoystickButton;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    shooterMotor.coastMotor();
+    topMotor.coastMotor();
+    bottomMotor.coastMotor();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    shooterMotor.setPower(joystickCommandXboxController.getLeftY());
+    topMotor.setPower(JoystickButton.buttonJoystickButton);
+    bottomMotor.setPower(JoystickButton.buttonJoystickButton);
+    //Don't know how to tell when pressed
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    shooterMotor.brakeMotor();
+    topMotor.brakeMotor();
+    bottomMotor.brakeMotor();
+  }
 
   // Returns true when the command should end.
   @Override
