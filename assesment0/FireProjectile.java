dcda5116e429b8b7bd7bddccdbbebd5e39e6bd8c
YaoPlaysMC <76790111+YaoPlaysMC@users.noreply.github.com>
2024-09-23 08:50:39 -0700

    Fixed the files

--- assesment0/FireProjectile.java
+++ assesment0/FireProjectile.java
@@ -2,14 +2,16 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package FireProjectile;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class FireProjectile extends Command {
   /** Creates a new FireProjectile. */
+  private ProjectileFirer myProjectileFirer;
   public FireProjectile() {
     // Use addRequirements() here to declare subsystem dependencies.
+    myProjectileFirer = new ProjectileFirer();
   }
 
   // Called when the command is initially scheduled.
@@ -17,8 +19,19 @@ public class FireProjectile extends Command {
   public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
+
+  //This function sets the angle first
   @Override
-  public void execute() {}
+  public void execute() {
+    //Sets up the proper angle
+    myProjectileFirer.setAngle();
+    //Speeds up the flywheel motor
+    myProjectileFirer.setFlywheelSpeed(1);
+    //Gives some time for the projectile to fly out
+    wait(50);
+    //Stops the motors
+    myProjectileFirer.setFlywheelSpeed(0);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
--- assesment0/ProjectileFirer.java
+++ assesment0/ProjectileFirer.java
@@ -4,14 +4,72 @@
 
 package frc.robot.commands;
 
+import javax.swing.plaf.basic.BasicRadioButtonMenuItemUI;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ProjectileFirer extends SubsystemBase {
   /** Creates a new ProjectileFirer. */
-  public ProjectileFirer() {}
+  //All the instance variables for the components
+  Joystick angleControlJoystick;
+  XboxController shooterController;
+  TalonFX angleMotor, topMotor, bottonMotor;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public ProjectileFirer() {
+    //Initializes the instance variables.
+    angleControlJoystick = new Joystick(4);
+    shooterController = new XboxController(5);
+    angleMotor = new TalonFX(1);
+    topMotor = new TalonFX(2);
+    bottonMotor = new TalonFX(3);
   }
+
+  //This function sets the angle of the angle motor based
+  public void adjustAngle() {
+    //This gets the position of the joystick 
+    //This probably isn't the best way to do so
+    //Maybe we need to do some math on the angle?
+    double newAngle = angleControlJoystick.getY();
+    //This sets the angle motor to the position
+    //This probably isn't the proper way to set the angle
+    angleMotor.setPosition(newAngle);
+  }
+
+  //This function sets the new speed of the motor
+  public void setFlywheelSpeed(double speed) {
+
+    //Sets the new speed of the flywheel motor
+    topMotor.set(speed);
+    bottonMotor.set(speed);
+  }
+  // @Override
+  // public void periodic() {
+  //   if (shooterController.getAButtonPressed()) {
+  //     angleMotor.set(0.1);
+  //     // angleMotor.setPosition(getCurrentAngle())
+  //   } 
+  //   if (shooterController.getAButtonReleased()) {
+  //     angleMotor.set(0);
+  //   } 
+  //   if (shooterController.getBButtonPressed()) {
+  //     angleMotor.set(-0.1);
+  //   } 
+  //   if (shooterController.getAButtonReleased()) {
+  //     angleMotor.set(0);
+  //   } 
+  // }
+
+  // public void setFlywheelSpeed(double speed) {
+  //   topMotor.set(speed);
+  //   bottonMotor.set(speed);
+  // }
+
+  // public int getCurrentAngle() {
+  //   return angleMotor.getPosition()
+  // }
 }
