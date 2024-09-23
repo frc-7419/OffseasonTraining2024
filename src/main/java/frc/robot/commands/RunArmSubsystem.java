c7e3353d1c5a796510f27680c2b9cc53e778b04b
YaoPlaysMC <76790111+YaoPlaysMC@users.noreply.github.com>
2024-09-23 08:55:02 -0700

    Update RunArmSubsystem.java

--- src/main/java/frc/robot/commands/RunArmSubsystem.java
+++ src/main/java/frc/robot/commands/RunArmSubsystem.java
@@ -25,12 +25,14 @@ public class RunArmSubsystem extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    arm.changeVoltage(power);
+    arm.coast();
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    arm.brake();
+  }
 
   // Returns true when the command should end.
   @Override
