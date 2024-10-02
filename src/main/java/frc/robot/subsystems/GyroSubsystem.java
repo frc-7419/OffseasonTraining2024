b7e41a912ea380e11b6c5fbae59353e495bd5080
Bachthoven <bachtoven.rules@gmail.com>
2024-10-02 14:12:14 -0700

    Update GyroSubsystem.java
    Problem importing gyro libraries, need to fix

--- src/main/java/frc/robot/subsystems/GyroSubsystem.java
+++ src/main/java/frc/robot/subsystems/GyroSubsystem.java
@@ -1,5 +1,28 @@
 package frc.robot.subsystems;
 
-public class GyroSubsystem {
-    
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.kauailabs.navx.frc.AHRS;
+
+public class GyroSubsystem extends SubsystemBase {
+
+    public GyroSubsystem() {
+        AHRS ahrs = new AHRS();
+    }
+
+    public double getGyroYaw() {
+    }
+
+    public double getGyroPitch() {
+    }
+
+    public double getGyroRoll() {  
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Gyro Yaw", getGyroYaw());
+        SmartDashboard.putNumber("Gyro Pitch", getGyroPitch());
+        SmartDashboard.putNumber("Gyro Roll", getGyroRoll());
+    }
 }
