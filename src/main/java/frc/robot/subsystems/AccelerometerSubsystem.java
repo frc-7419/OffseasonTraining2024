7fbe701635ebe2398a169214cd225730d4efc404
VivianS27 <143737473+VivianS27@users.noreply.github.com>
2024-10-02 18:47:34 -0700

    Update AccelerometerSubsystem.java

--- src/main/java/frc/robot/subsystems/AccelerometerSubsystem.java
+++ src/main/java/frc/robot/subsystems/AccelerometerSubsystem.java
@@ -10,11 +10,21 @@ import com.kauailabs.navx.frc.AHRS;
 
 
 public class AccelerometerSubsystem extends SubsystemBase {
+  /*We usually need the analog import, and specifiy which one
+  , but since we are using NAVX we don't need to */
   /** Creates a new AccelerometerSubsystem. */
   private final AHRS ahrs;
+  /*private final because we don't want to change it */
 
   public AccelerometerSubsystem() {
     ahrs = new AHRS();
+    /* ahrs: altituteheadingreferencesystem: An attitude and heading reference system (AHRS) is a device that 
+     provide estimation of an objects and orientation in, can integrate accelerometer 
+     Measurements of pitch, roll, and yaw are typical data outputs.
+     pitch: Side-to-side axis rotation(y axis)
+     roll: Front-to-back axis rotation(x axis)
+     yaw: Rotation around vertical axis
+     */
     
   }
 
@@ -24,18 +34,21 @@ public class AccelerometerSubsystem extends SubsystemBase {
     return ahrs.getYaw();
   }
 
+  //Acceleration on X axis
   public double getAccelX() {
     return ahrs.getWorldLinearAccelX();
   }
 
+  //Acceleration on Y axis
   public double getAccelY() {
     return ahrs.getWorldLinearAccelY();
   }
 
+  //Accleration on XY axis
   public double getAccelXY() {
     return Math.sqrt(Math.pow(getAccelX(), 2) + Math.pow(getAccelY(), 2));
   }
-
+  //Compass Heading: Angle relative to north
   public double getCompassHeading() {
     return ahrs.getCompassHeading();
   }
@@ -51,12 +64,14 @@ public class AccelerometerSubsystem extends SubsystemBase {
     return Math.sqrt(Math.pow(getVelocityX(), 2) + Math.pow(getVelocityY(), 2));
   }
 
+  //reset
   public void reset() {
     ahrs.reset();
   }
 
   @Override
   public void periodic() {
+    //displays values on SmartDashboard
     SmartDashboard.putNumber("Yaw", getYaw());
     SmartDashboard.putNumber("Accel X", getAccelX());
     SmartDashboard.putNumber("Accel Y", getAccelY());
