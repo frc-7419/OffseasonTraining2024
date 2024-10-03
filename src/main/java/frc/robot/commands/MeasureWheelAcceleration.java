0ab5bfbfefe0b742c951e9b34770fa8c0cff8e70
VivianS27 <143737473+VivianS27@users.noreply.github.com>
2024-10-02 20:28:24 -0700

    Made fixes

--- src/main/java/frc/robot/commands/MeasureWheelAcceleration.java
+++ src/main/java/frc/robot/commands/MeasureWheelAcceleration.java
@@ -1,41 +1,44 @@
 package frc.robot.commands;
 
-import frc.robot.Subsystems.AcceleroemeterSubsystem;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.AccelerometerSubsystem;
+import frc.robot.subsystems.WheelSubsystem;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class MeasureAcceleration extends CommandBase {
+public class MeasureWheelAcceleration extends CommandBase {
   private final AccelerometerSubsystem accelerometerSubsystem;
+  private final WheelSubsystem wheelMotor;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public MeasureAcceleration(AccelerometerSubsystem accelerometerSubsystem) {
+  public MeasureWheelAcceleration(AccelerometerSubsystem accelerometerSubsystem) {
     this.accelerometerSubsystem = accelerometerSubsystem;
-    
-    addRequirements(acceleometerSubsystem);
+    this.wheelMotor = wheelMotor;
+    addRequirements(accelerometerSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
-
-    
+    //set motor to neutral
+    wheelMotor.coast();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    acclerometerSubsystem.periodic();
-    
+    //get velocity and run the motor
+    wheelMotor.runMotor(0.5);
+    wheelMotor.getMotorVelocity();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    
+    //stop the motor
+    wheelMotor.brake();
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/subsystems/WheelSubsystem.java
+++ src/main/java/frc/robot/subsystems/WheelSubsystem.java
@@ -3,6 +3,41 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-/** Add your docs here. */
-public class WheelMotor {}
+public class WheelSubsystem extends SubsystemBase {
+  //new wheel Motor
+  private final TalonFX wheelMotor;
+
+  public WheelSubsystem() {
+    this.wheelMotor = new TalonFX(0);
+  }
+
+  //run the motor
+  public void runMotor(double power) {
+    wheelMotor.set(power);
+    
+  }
+  //get velocity
+  public void getMotorVelocity(){
+    wheelMotor.getVelocity();
+  }
+
+  public void coast() {
+    wheelMotor.setNeutralMode(NeutralModeValue.Coast);
+  }
+
+  public void brake() {
+    wheelMotor.setNeutralMode(NeutralModeValue.Brake);
+  }
+
+  @Override
+  //show on smart dashboard
+  public void periodic() {
+    SmartDashboard.putNumber("WheelMotor velocity", wheelMotor.getVelocity().getValue());
+    SmartDashboard.putNumber("wheelMotor voltage", wheelMotor.getMotorVoltage().getValue());
+  }
+}
