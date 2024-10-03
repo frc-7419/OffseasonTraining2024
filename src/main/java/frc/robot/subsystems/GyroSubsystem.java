3ea3aaebf3e1317fec49f00b88085148eadd6cfe
humanifying9 <tiaparekh9@gmail.com>
2024-10-02 21:53:55 -0700

    adding in return values periodically

--- src/main/java/frc/robot/subsystems/GyroSubsystem.java
+++ src/main/java/frc/robot/subsystems/GyroSubsystem.java
@@ -3,16 +3,41 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class GyroSubsystem extends SubsystemBase {
-  private 
+
+  WPI_PigeonIMU gyro = new WPI_PigeonIMU(0); 
+  TalonSRX talon = new TalonSRX(0); // TalonSRX is on CAN Bus with device ID 0
+  WPI_PigeonIMU gyro = new WPI_PigeonIMU(talon); // Pigeon uses the talon created above
+  
+  private final Pigeon2 gyroPigeon;
   /** Creates a new GyroPresentationSubsystem. */
-  public GyroPresentationSubsystem() {}
+  public GyroSubsystem() {}
+
+  public double getGyroYaw() {
+
+  }
+
+  public double getGyroPitch() {
+
+  }
+
+  public double getGyroRoll() {
+    
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Gyro Yaw", getGyroYaw());
+    SmartDashboard.putNumber("Gyro Pitch", getGyroPitch());
+    SmartDashboard.putNumber("Gyro Roll", getGyroRoll());
   }
+
+  public double yawARHS() {
+
+  }
+  
 }
