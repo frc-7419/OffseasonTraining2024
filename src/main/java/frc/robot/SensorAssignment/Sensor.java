a0779a26c1ae2b963e8156023d7455d632ce9816
CryptoSOL123 <163804105+CryptoSOL123@users.noreply.github.com>
2024-10-03 16:02:13 -0700

    Made the sensor

--- src/main/java/frc/robot/SensorAssignment/Sensor.java
+++ src/main/java/frc/robot/SensorAssignment/Sensor.java
@@ -4,14 +4,49 @@
 
 package frc.robot.SensorAssignment;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Sensor extends SubsystemBase {
+  private final TalonFX moveMotorOne;
+  private final TalonFX moveMotorTwo;
+  AnalogPotentiometer pot = new AnalogPotentiometer(0, 180, 30);
   /** Creates a new Sensor. */
-  public Sensor() {}
+  public Sensor(TalonFX moveMotorOne, TalonFX moveMotorTwo) {
+    this.moveMotorOne = moveMotorOne;
+    this.moveMotorTwo = moveMotorTwo;
+  }
 
+
+  public void coast(){
+    moveMotorOne.setNeutralMode(NeutralModeValue.Coast);
+    moveMotorTwo.setNeutralMode(NeutralModeValue.Coast);
+  }
+
+  public void brake(){
+    moveMotorOne.setNeutralMode(NeutralModeValue.Brake);
+    moveMotorTwo.setNeutralMode(NeutralModeValue.Brake);
+  }
+
+  public void setPower(double speed){
+    moveMotorOne.set(speed);
+    moveMotorTwo.set(speed);
+  }
+
+
+  public double getSensorDistance(){
+    return (pot.get());
+
+  }
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("Distance from Target", 0)
     // This method will be called once per scheduler run
   }
 }
