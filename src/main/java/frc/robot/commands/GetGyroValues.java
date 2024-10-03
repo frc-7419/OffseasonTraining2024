f0adcbfa192b755d23189bfaf60ca3a5910c1c7c
humanifying9 <tiaparekh9@gmail.com>
2024-10-03 14:55:43 -0700

    Update GetGyroValues.java

--- src/main/java/frc/robot/commands/GetGyroValues.java
+++ src/main/java/frc/robot/commands/GetGyroValues.java
@@ -3,26 +3,48 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.GyroSubsystem;
 
-public class GetGyroValues extends Command {
+public class GetGyroValues extends Command 
+  private final GyroSubsystem gyroSubsystem;
+  private final CommandXboxController commandXboxController;
+  private final ArmSubsystem armSubsystem;
+
+
   /** Creates a new GetGyroValues. */
-  public GetGyroValues() {
+  public GetGyroValues(GyroSubsystem gyroSubsystem,CommandXboxController commandXboxController,ArmSubsystem armSubsystem) {
+    this.gyroSubsystem = gyroSubsystem;
+    this.commandXboxController = commandXboxController;
+    this.armSubsystem = armSubsystem;
+
+    addRequirements(gyroSubsystem, armSubsystem);
+
+    
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    armSubsystem.coast();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if (gyroSubsystem.yaw()>= 20){
+      armSubsystem.runMotor(0.5);
+    }
+    
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    armSubsystem.brake();
+  }
 
   // Returns true when the command should end.
   @Override
