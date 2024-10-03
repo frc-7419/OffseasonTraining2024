28dfaab6ff1f9e1acda7d3be76e357bf5d1a9a36
Bachthoven <bachtoven.rules@gmail.com>
2024-10-02 22:10:07 -0700

    Finished coding the Gyro subsystem

--- src/main/java/frc/robot/subsystems/GyroSubsystem.java
+++ src/main/java/frc/robot/subsystems/GyroSubsystem.java
@@ -5,18 +5,22 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.kauailabs.navx.frc.AHRS;
 
 public class GyroSubsystem extends SubsystemBase {
+    private final AHRS ahrs;
 
     public GyroSubsystem() {
-        AHRS ahrs = new AHRS();
+        this.ahrs = new AHRS();
     }
 
     public double getGyroYaw() {
+        return ahrs.getYaw();
     }
 
     public double getGyroPitch() {
+        return ahrs.getPitch();
     }
 
-    public double getGyroRoll() {  
+    public double getGyroRoll() { 
+        return ahrs.getRoll(); 
     }
 
     @Override
