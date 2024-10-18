a96bb6de1567bcf4c528eedeb00f008ca1415bdd
mqxs21 <maxshi239@gmail.com>
2024-10-17 18:29:51 -0700

    pid tuning

--- src/main/java/frc/robot/Commands/SetArmToPoint.java
+++ src/main/java/frc/robot/Commands/SetArmToPoint.java
@@ -6,13 +6,18 @@ package frc.robot.Commands;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Subsystems.ArmSubsystem;
 
 public class SetArmToPoint extends Command {
-  private final PIDController pidController = new PIDController(1.0,0.0,0.0);
+  private double kP = 1;
+  private double kD = 0;
+  private final PIDController pidController = new PIDController(kP,0.0,kD);
   private double setPoint;
   private final ArmSubsystem armSubsystem;
+  public double p = 1;
+  public double d = 1;
   private final ArmFeedforward armFeedforward = new ArmFeedforward(0.0,1.0,0.5,0.1);
   public SetArmToPoint(ArmSubsystem armSubsystem, double setPoint) {
     this.armSubsystem = armSubsystem;
@@ -35,6 +40,12 @@ public class SetArmToPoint extends Command {
     double currentPosition = armSubsystem.getPosition();
     double ff = armFeedforward.calculate(currentPosition,5);
     armSubsystem.setPower(ff + pidController.calculate(currentPosition));
+    SmartDashboard.putNumber("P value", p);
+    p = SmartDashboard.getNumber("P value", kP);
+    SmartDashboard.putNumber("D value", d);
+    d = SmartDashboard.getNumber("D value", kD);
+    pidController.setP(p);
+    pidController.setD(d);
 
   }
 
