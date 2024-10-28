990a7f79a259e8557a591120ef2181957fdf9310
shlokacharya <acharya.shlok@gmail.com>
2024-10-28 16:06:30 -0700

    Change in import names bc CAPS

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -2,12 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Commands;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.Subsystems.DriveBaseSubsystem;
 import frc.robot.subsystems.DriveBaseSubsystem;
 
 public class ArcadeDrive extends Command {
