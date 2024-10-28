441b2db871e2209d8fc352ccd60076164aac7ee0
shlokacharya <acharya.shlok@gmail.com>
2024-10-28 15:54:42 -0700

    Drivebase Subsystem + Arcade Drive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,13 @@
 package frc.robot;
 
 public class Constants {
+    public static final class CANIds {
+        // These are all arbitrary values for the CANIDs
+        public static final int leftFalcon1 = 1; 
+        public static final int leftFalcon2 = 2; 
+        public static final int rightFalcon1 = 3;
+        public static final int rightFalcon2 = 4;
+    }
     public class Operator {
         public static final int kdriveControllerPort = 0;
     }
---
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Subsystems.DriveBaseSubsystem;
+import frc.robot.subsystems.DriveBaseSubsystem;
+
+public class ArcadeDrive extends Command {
+
+  private final DriveBaseSubsystem driveBaseSubsystem;
+  private final XboxController xboxController;
+
+  public ArcadeDrive(DriveBaseSubsystem driveBaseSubsystem, XboxController xboxController) {
+    this.driveBaseSubsystem = driveBaseSubsystem;
+    this.xboxController = xboxController;
+    addRequirements(driveBaseSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    double forward = -xboxController.getLeftY();
+    double rotation = xboxController.getRightX();
+    forward *= 0.5;
+    rotation *= 0.5;
+
+    driveBaseSubsystem.setPower(forward + rotation, forward - rotation);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveBaseSubsystem.setPower(0,0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
