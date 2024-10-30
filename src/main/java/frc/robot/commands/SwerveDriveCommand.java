81cbc5f48a67d77f0b129609de2e54f93fe16e5b
shlokacharya <acharya.shlok@gmail.com>
2024-10-30 11:51:02 -0700

    Not Using Deprecated Stuff

--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -1,10 +1,10 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 import edu.wpi.first.wpilibj.XboxController;
 
-public class SwerveDriveCommand extends CommandBase {
+public class SwerveDriveCommand extends Command {
     private final XboxController controller;
     private final SwerveDriveSubsystem swerveDriveSubsystem;
 
