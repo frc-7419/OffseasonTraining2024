31aa1794c65b3bf81da3335fde12717226123595
CryptoSOL123 <pradyun4050@gmail.com>
2024-11-04 17:05:25 -0800

    Update DriveControl.java

--- src/main/java/frc/robot/arcadeDrive/DriveControl.java
+++ src/main/java/frc/robot/arcadeDrive/DriveControl.java
@@ -6,7 +6,6 @@ package frc.robot.Commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Subsystems.ArcadeDrive;
 
 public class DriveControl extends Command {
   private final arcadeDriveSubsystem arcadeDrive;
