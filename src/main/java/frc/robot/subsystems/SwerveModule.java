f2892633df0c93eea5dcb74e75bf42f875bb9d42
VivianS27 <143737473+VivianS27@users.noreply.github.com>
2024-11-11 22:56:11 -0800

    Swerve, Auton 2 note

---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,41 @@
+//needs review-
+package frc.robot.subsystems;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.Encoder;
+
+public class SwerveModule {
+    private Spark driveMotor;
+    private Spark steerMotor;
+    private Encoder encoder;
+    private PIDController PID;
+    private double currentAngle;
+
+    // Constructor
+    public SwerveModule(int driveMotorId, int steerMotorId, int encoderA, int encoderB, double P, double I, double D) {
+        this.driveMotor = new Spark(driveMotorId);
+        this.steerMotor = new Spark(steerMotorId);
+        this.encoder = new Encoder(encoderA, encoderB); 
+        this.PID = new PIDController(P, I, D);
+        this.currentAngle = 0; 
+    }
+
+    public void drive(double speed, double targetAngle) {
+        driveMotor.set(speed);
+
+        // Update the current angle from the encoder
+        currentAngle = encoder.get(); // Adjust this if you have a different scale
+
+        // Calculate the steering adjustment using PID
+        double steerOutput = PID.calculate(currentAngle, targetAngle);
+        steerMotor.set(steerOutput);
+    }
+
+    public void setCurrentAngle(double angle) {
+        this.currentAngle = angle; // Update the current angle
+    }
+
+    public double getCurrentAngle() {
+        return currentAngle; // Get the current angle
+    }
+}
