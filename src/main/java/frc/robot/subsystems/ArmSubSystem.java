d5005e87abfbf55ce061a0a0b65714f18a67ea98
Amrit-s-RIS <therealamritsingh@gmail.com>
2024-11-12 22:15:55 -0800

    Revert "Update ArmSubSystem.java"
    This reverts commit e7049503cdc02157568716ab851db3b27f6b9d71.

--- src/main/java/frc/robot/subsystems/ArmSubSystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubSystem.java
@@ -7,27 +7,40 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubSystem extends SubsystemBase {
-  private final TalonFX armMotor;
-  /** Creates a new ArmSubsystem. */
+  /** Creates a new ArmSubSystem. */
+  private final TalonFX armMotor; // defines motor
+  DutyCycleEncoder encoder = new DutyCycleEncoder(0);
+
   public ArmSubSystem() {
-    this.armMotor = new TalonFX(0);
+    this.armMotor = new TalonFX(0); //creates a new object of type talonfx with deviceID 0
+  }
+  public void set(double power){ //constructor for the runMotor() command that accepts parameter of power
+    armMotor.set(power);
+
 
   }
-  
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-  public void brake() {
-    armMotor.setNeutralMode(NeutralModeValue.Brake);
-  }
-  public void coast() {
+
+  public void coast() { //coast command method that sets neutral mode value to coast
     armMotor.setNeutralMode(NeutralModeValue.Coast);
+
   }
-  public void runArm() {
-    armMotor.set(0);
-  }     
+  public void brake() { //same as before but sets to brake
+    armMotor.setNeutralMode(NeutralModeValue.Brake);
+
+  }
+  @Override
+  public void periodic() { //dont know what this does but my best guess is that it displays voltage.
+    SmartDashboard.putNumber("Arm motor Voltage", armMotor.getMotorVoltage().getValue());
+  }
+  public double getPosition() {
+    return encoder.getAbsolutePosition() - encoder.getPositionOffset();
+
+
+
+  }
 }
